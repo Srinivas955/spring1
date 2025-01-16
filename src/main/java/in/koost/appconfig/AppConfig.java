6a688981eda5515a878c222c0fb29d1ab9e97768
Srinivas Yadav <srinivasyadav@gmail.com>
2025-01-16 17:16:08 +0530

    updated primary and qualifier annotation to prioritize to create bean code

--- src/main/java/in/koost/appconfig/AppConfig.java
+++ src/main/java/in/koost/appconfig/AppConfig.java
@@ -3,24 +3,34 @@ package in.koost.appconfig;
 import in.koost.Alien;
 import in.koost.Computer;
 import in.koost.Desktop;
+import in.koost.Laptop;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
-    //@Bean(name ="desktop")
-   // @Bean(name={"desktop", "beast"})
-    @Bean
-    @Scope(value = "prototype")
-    public Desktop desktop(){ //by default method name is bean name
-        return new Desktop();
-    }
+
+   /* @Bean
+    public Alien alien(@Qualifier("laptop") Computer comp){ //to create the bean of laptop by using qualifier
+                                                            //annotation when when morethan one classes
+                                                            //implement the interface
+        Alien alien = new Alien();
+        alien.setAge(33);
+        //alien.setComputer(desktop()); //which is tightly coupled
+        alien.setComputer(comp);
+
+        return alien;
+    }*/
+
+
 
     @Bean
-    public Alien alien(@Autowired Computer comp){ //we are creating a wire between an Alien and desktop object
+    public Alien alien(@Qualifier("laptop") @Autowired Computer comp){ //we are creating a wire between an Alien and desktop object
                                                     // Computer is an interface and desktop its implementation
                                                     //class
         Alien alien = new Alien();
@@ -29,7 +39,20 @@ public class AppConfig {
         alien.setComputer(comp);
 
         return alien;
+    }
 
+    //@Bean(name ="desktop")
+    // @Bean(name={"desktop", "beast"})
+    @Bean
+    @Scope(value = "prototype")
+    public Desktop desktop(){ //by default method name is bean name
+        return new Desktop();
+    }
 
+    @Bean
+    @Primary  //using primary annotation at class level to prioritize when more than one classes
+             //implementing on interface
+    public Laptop laptop(){
+     return new Laptop();
     }
 }
