4ad3bc987b7878a16d2821a795c0762f011729b7
Srinivas Yadav <srinivasyadav@gmail.com>
2025-01-16 17:02:03 +0530

    updated code for autowired annotation

--- src/main/java/in/koost/appconfig/AppConfig.java
+++ src/main/java/in/koost/appconfig/AppConfig.java
@@ -1,6 +1,9 @@
 package in.koost.appconfig;
 
+import in.koost.Alien;
+import in.koost.Computer;
 import in.koost.Desktop;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -14,5 +17,19 @@ public class AppConfig {
     @Scope(value = "prototype")
     public Desktop desktop(){ //by default method name is bean name
         return new Desktop();
+    }
+
+    @Bean
+    public Alien alien(@Autowired Computer comp){ //we are creating a wire between an Alien and desktop object
+                                                    // Computer is an interface and desktop its implementation
+                                                    //class
+        Alien alien = new Alien();
+        alien.setAge(33);
+        //alien.setComputer(desktop()); //which is tightly coupled
+        alien.setComputer(comp);
+
+        return alien;
+
+
     }
 }
